d2d9ea2f5f62ff5bbda1db0dc741d76b888d48a1
James Stalley-Moores <james.moores@eedi.co.uk>
2022-05-03 08:18:59 +0100

    Add project files.

---
+++ SlackApi/Program.cs
@@ -0,0 +1,34 @@
+using SlackApi;
+
+// https://andrewlock.net/exploring-dotnet-6-part-12-upgrading-a-dotnet-5-startup-based-app-to-dotnet-6/
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Manually create an instance of the Startup class
+var startup = new Startup(builder.Configuration, builder.Environment);
+
+// Manually call ConfigureServices()
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ SlackApi/Startup.cs
@@ -0,0 +1,43 @@
+﻿using SlackApi.App.Settings;
+
+namespace SlackApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
+        public const string CorsPolicyName = "PermittedOrigins";
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _env = env;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var applicationSettings = GetApplicationSettings();
+            var connectionString = GetConnectionStrings();
+
+
+        }
+
+        #region private methods
+
+        private ApplicationSettings GetApplicationSettings()
+        {
+            var applicationSettings = new ApplicationSettings();
+            Configuration.Bind("AppSettings", applicationSettings);
+            return applicationSettings;
+        }
+
+        private ConnectionStrings GetConnectionStrings()
+        {
+            var connectionStrings = new ConnectionStrings();
+            Configuration.Bind("ConnectionStrings", connectionStrings);
+            return connectionStrings;
+        }
+
+        #endregion
+    }
+}
